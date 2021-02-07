ca5713a07ec00b457f2edc1548baa69d51bb5d5b
$taneD3v <78273896+Stane034@users.noreply.github.com>
2021-02-07 18:03:13 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.Color         = { r = 0, g = 128, b = 255 }
 Config.Type          = 1
 
 Config.Locale        = 'en'
-
+Config.Webhook 		 = '' -- There put your webhook
 Config.LicenseEnable = true -- only turn this on if you are using esx_license
 Config.LicensePrice  = 5000
 
