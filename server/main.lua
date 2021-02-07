55bc37eecd624e5906efa8072002142292e1acc2
$taneD3v <78273896+Stane034@users.noreply.github.com>
2021-02-07 18:02:08 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -45,50 +45,63 @@ end)
 
 ESX.RegisterServerCallback('esx_weaponshop:buyWeapon', function(source, cb, weaponName, zone)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local price = GetPrice(weaponName, zone)
+	local price = 0
+	print(ESXBalkan)
+	for i=1, #shopItems[zone], 1 do
+		if shopItems[zone][i].item == weaponName then
+			price = shopItems[zone][i].price
+			break
+		end
+	end
 
 	if price == 0 then
 		print(('esx_weaponshop: %s attempted to buy a unknown weapon!'):format(xPlayer.identifier))
 		cb(false)
+	end
+
+	if xPlayer.hasWeapon(weaponName) then
+		TriggerClientEvent('esx:showNotification', source, _U('already_owned'))
+		cb(false)
 	else
-		if xPlayer.hasWeapon(weaponName) then
-			xPlayer.showNotification(_U('already_owned'))
-			cb(false)
-		else
-			if zone == 'BlackWeashop' then
-				if xPlayer.getAccount('black_money').money >= price then
-					xPlayer.removeAccountMoney('black_money', price)
-					xPlayer.addWeapon(weaponName, 42)
-	
-					cb(true)
-				else
-					xPlayer.showNotification(_U('not_enough_black'))
-					cb(false)
-				end
+		if zone == 'BlackWeashop' then
+
+			if xPlayer.getAccount('bank').money >= price then
+				xPlayer.removeAccountMoney('bank', price)
+				xPlayer.addWeapon(weaponName, 42)
+
+				cb(true)
 			else
-				if xPlayer.getMoney() >= price then
-					xPlayer.removeMoney(price)
-					xPlayer.addWeapon(weaponName, 42)
-	
-					cb(true)
-				else
-					xPlayer.showNotification(_U('not_enough'))
-					cb(false)
-				end
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_black'))
+				cb(false)
 			end
+
+		else
+
+			if xPlayer.getMoney() >= price then
+				xPlayer.removeMoney(price)
+				xPlayer.addWeapon(weaponName, 42)
+				posaljinaDiscord("Weaponshop " .. xPlayer.getName() .. " bought " .. weaponName)
+				cb(true)
+			else
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough'))
+				cb(false)
+			end
+	
 		end
 	end
 end)
 
-function GetPrice(weaponName, zone)
-	local price = MySQL.Sync.fetchScalar('SELECT price FROM weashops WHERE zone = @zone AND item = @item', {
-		['@zone'] = zone,
-		['@item'] = weaponName
-	})
-
-	if price then
-		return price
-	else
-		return 0
-	end
-end
+function posaljinaDiscord(name, message)
+	local vremeporuke = os.date('*t')
+	local poruka = {
+		{
+			["color"] = 16711680,
+			["title"] = "**".. name .."**",
+			["description"] = message,
+			["footer"] = {
+			["text"] = "Logovi\nVreme: " .. vremeporuke.hour .. ":" .. vremeporuke.min .. ":" .. vremeporuke.sec,
+			},
+		}
+	  }
+	PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode({username = "Logovi", embeds = poruka, avatar_url = "https://cdn.discordapp.com/attachments/732353958349242460/764825950458085414/IMG_20201011_123627_852.jpg"}), { ['Content-Type'] = 'application/json' })
+  end
